53b73edfdec61c30f3c4b57534611aace1ad91a9
Francisco Salinas <fcosalinas1987@gmail.com>
2013-10-15 11:16:01 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Muuk::Application.config.secret_key_base = 'e440fd756096e1817532bdd75ec89f80c49cbc5b94187cc3e84582f57165b963fffbdb7fe2db996fc1f3add8e42c8096c85a447c135491b19b25faf2f2d329cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Muuk::Application.config.session_store :cookie_store, key: '_Muuk_session'
