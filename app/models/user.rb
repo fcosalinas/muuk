47c04d2821a9d3ee87d0eb8f2ade738740e09702
Francisco Salinas <fcosalinas1987@gmail.com>
2013-10-27 18:50:19 -0300

    modificando modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+
+	def formatted_email
+		"#{full_name} <#{email}>"
+	end
+
+	def full_name
+		return first_name + " " + last_name
+	end
 end
