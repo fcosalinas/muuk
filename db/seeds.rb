2477d94e663c5002d2bd05c3dbb1b10e05a73311
Francisco Salinas <fcosalinas1987@gmail.com>
2013-10-27 16:38:04 -0300

    creadas las seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.delete_all
-user1 = User.create(first_name: 'Sebastián', last_name:'Lewin', email: 'slewinr@gmail.com', birth_date: '1987-04-21', city:'Santiago', country: 'Chile', address: 'Cardenal Newman 667', gender:'Female')
-user2 = User.create(first_name: 'María Victoria', last_name: 'Kash', email: 'mariapuppe@deutsch.de', birth_date: '1993-11-22', city: 'München', country:'Deutschland', address: 'Ja Ja 666', gender: 'Unknown')
+#User.delete_all
+#user1 = User.create(first_name: 'Sebastián', last_name:'Lewin', email: 'slewinr@gmail.com', birth_date: '1987-04-21', city:'Santiago', country: 'Chile', address: 'Cardenal Newman 667', gender:'Female')
+#user2 = User.create(first_name: 'María Victoria', last_name: 'Kash', email: 'mariapuppe@deutsch.de', birth_date: '1993-11-22', city: 'München', country:'Deutschland', address: 'Ja Ja 666', gender: 'Unknown')
